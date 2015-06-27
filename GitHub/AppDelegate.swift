ef40683909bf387580b3328645f9eaa32ae8bab8
Joshua Truscott <joshtruscott95@gmail.com>
2015-06-27 15:02:54 +1000

    Revert 44b6bf8..42357fa
    This rolls back to commit 44b6bf869e3719e988a153376dab86f48070ec5f.

--- GitHub/AppDelegate.swift
+++ GitHub/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Overrinhrtjw this is 2 de point for customization after application launch.
         return true
     }
 
